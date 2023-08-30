53a4c4ac46d380f2bd38cb8a848c703ef8a7b1c8
SerDunk <abhinavkondapalli7@gmail.com>
2023-08-30 23:06:57 +0530

    Added minor effects like hover and scroll-effect to index.html and finished work on terms and conditions side page with full customisation

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll",function(){
-    let header=document.querySelector("header")
-    header.classList.toggle('sticky',window.scrollY>0)
-})
-AOS.init({
-    duration: 800, // Animation duration
-    once: true, // Only animate elements once
-});
