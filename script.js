957972d573fd3928cfad639a2b0efb91c925ba59
SerDunk <abhinavkondapalli7@gmail.com>
2023-08-30 17:58:39 +0530

    Primary website of skill-brew constructed with css styles for most sections

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ window.addEventListener("scroll",function(){
     let header=document.querySelector("header")
     header.classList.toggle('sticky',window.scrollY>0)
 })
+AOS.init({
+    duration: 800, // Animation duration
+    once: true, // Only animate elements once
+});
